04d061b16b7e62d456308e4b8eb3d64e23875c84
chea <christorichard83@gmail.com>
2022-08-28 01:42:30 +0700

    test

--- login.php
+++ login.php
@@ -6,8 +6,8 @@
 
     $sql = "SELECT * FROM tbAkun WHERE Username='".$username."'AND Password='".$password."'";
     $result = sqlsrv_query($conn, $sql);
-    $sql = "SELECT TipeAkun FROM tbAkun WHERE Username='".$username."'AND Password='".$password."'";
-    $result2 = sqlsrv_query($conn, $sql);
+    $sql2 = "SELECT TipeAkun FROM tbAkun WHERE Username='".$username."'AND Password='".$password."'";
+    $result2 = sqlsrv_query($conn, $sql2);
     if($result){
         if($result2=='Admin'){
             header("Location:Admin/page1.php?q=".$username);
