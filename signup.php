e2aff2f680d82ca451cb7e97b575b95ac27bf1cc
chea <christorichard83@gmail.com>
2022-08-28 02:34:23 +0700

    username gabole sama di signup

--- signup.php
+++ signup.php
@@ -14,18 +14,23 @@
     $jumlahBaris = sqlsrv_num_rows( $resultUser );
     $namaUser = sqlsrv_get_field( $resultUser2, 3);
 
+    //Jika $jumlahBaris = 0, maka tidak ada username yang sama
     if($jumlahBaris==0){
+        //input data
         $sql = "EXEC [dbo].[SP_AutoAkun] '".$firstName."','".$lastName."','".$userName."','".$phone."','".$password."','User'";
         $result = sqlsrv_query( $conn, $sql );
         if($result){
+            //Data berhasil diinput
             echo '<script type ="text/JavaScript">alert("Akun Berhasil Dibuat")</script>';
             header("Refresh:0; url=index.html");
         }
         else{
+            //Data gagal input
             echo '<script type ="text/JavaScript">alert("Akun Gagal Dibuat") </script>';
             header("Refresh:0; url=index.html");
         }
-    } 
+    }
+    //Data gagal input karena username sama
     else{
         echo '<script type ="text/JavaScript">alert("Akun Gagal Dibuat Karena Username sama!") </script>';
         header("Refresh:0; url=index.html");
